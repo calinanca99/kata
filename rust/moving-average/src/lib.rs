298a3ea58eed686b67ad1cf146cefa01cf1e6c00
calinanca99 <calinanca99@gmail.com>
2022-10-18 08:42:07 +0200

    Make conversion from usize to f64 safe

--- rust/moving-average/src/lib.rs
+++ rust/moving-average/src/lib.rs
@@ -1,34 +1,35 @@
+use conv::ValueFrom;
+
 /// Returns a vector whose elements represent the arithmetic mean of each n
-/// consecutive elements from `array` (i.e.: a window), where n == `window_size`.
+/// consecutive elements from `array` (i.e.: a window).
 ///
 ///
 /// ## Notes
+/// - If the window size is larger than the length of the array, the window size
+/// becomes the length of the array => MIN(`n`, `array.len()`) is used
+/// as window size.
+///
 /// - If the input array is empty `None` is returned.
 ///
-/// - If the window size is larger than the length of the array, the window size
-/// becomes the length of the array.
+/// - If the window size cannot be safely converted to `f64`
+///  then `None` is returned.
 ///
 /// - If one of the sums of a window overflows then the average for that window is
 /// infinity (`f64::INFINITY`).
-pub fn moving_average(array: &[f64], window_size: usize) -> Option<Vec<f64>> {
-    if array.is_empty() {
+pub fn moving_average(array: &[f64], n: usize) -> Option<Vec<f64>> {
+    let window_size = usize::min(n, array.len());
+
+    if array.is_empty() || f64::value_from(window_size).is_err() {
         return None;
     }
-
-    let array_length = array.len();
-
-    let window_size = if window_size > array_length {
-        array_length
-    } else {
-        window_size
-    };
 
     Some(array.windows(window_size).map(average).collect::<Vec<_>>())
 }
 
-fn average(array: &[f64]) -> f64 {
-    let len = array.len() as f64;
-    let sum = array.iter().sum::<f64>();
+fn average(window: &[f64]) -> f64 {
+    // `unwrap()` is safe since condition has been checked previously
+    let len = f64::value_from(window.len()).unwrap();
+    let sum = window.iter().sum::<f64>();
 
     sum / len
 }
@@ -77,6 +78,23 @@ mod tests {
         // Assert
         assert!(sut.is_none())
     }
+
+    // Test fails due to the following error:
+    //
+    // `error: values of the type `[f64; 18446744073709551615]` are too big for the current architecture`
+    //
+    // #[test]
+    // fn returns_none_when_window_size_cannot_be_converted_to_f64() {
+    //     // Arrange
+    //     let array = [0.0; usize::MAX];
+    //     let window = usize::MAX;
+
+    //     // Act
+    //     let sut = moving_average(&array, window);
+
+    //     // Assert
+    //     assert!(sut.is_none())
+    // }
 
     #[test]
     fn uses_length_of_array_as_window_size_when_window_size_is_greater_than_length_of_the_array() {
